b3bf717fdc0c56e61babbd2f4e50eb7c3b8d6c3a
Mikey <manchung.wu@mail.utoronto.ca>
2022-02-18 18:53:37 -0500

    feat: add loading state animations fix:  color for security price change is green when prices increase and red when prices drop

--- src/components/quote/Quote.vue
+++ src/components/quote/Quote.vue
@@ -8,24 +8,34 @@
                     v-on:search="searchSecurity"
                     v-model="searchKeywords"></search-bar>
         <hr class="bg-dark my-2">
-        <div v-show="errorMsg">
-            <h6 class="text-info text-center">{{ errorMsg }}</h6>
-        </div>
-        <div v-show="showResult && !errorMsg">
-            <h6 v-show="searchResult.Name" class="text-black-50">{{ searchResult.Name }} ({{ searchResult.Symbol }})</h6>
-            <div class="d-flex justify-content-start align-items-end mb-1 prices">
-                <h2 class="mr-3 mb-n1">{{ searchResult.LastPrice }}</h2>
-                <h5 class="text-success mr-1 mb-0">{{ searchResult.Change }}</h5>
-                <h5 v-show="searchResult.ChangePercent" class="text-success mb-0">({{ searchResult.ChangePercent }})</h5>
+        <div class="position-relative contents">
+            <div v-show="errorMsg">
+                <h6 class="text-info text-center">{{ errorMsg }}</h6>
             </div>
-            <p class="text-grey font-weight-custom timestamp">{{ searchResult.Timestamp }}</p>
-            <hr class="bg-light mt-2 mb-1">
-            <div v-for="{ item, key } in detailItems" v-bind:key="item">
-                <div class="d-flex justify-content-between align-items-center px-1">
-                    <small class="text-grey font-weight-custom">{{ item }}</small>
-                    <small class="font-weight-bold">{{ searchResult[key] }}</small>
+            <div v-if="searching"
+                 class="position-absolute w-100 h-100 d-flex justify-content-center align-items-center overlay">
+                <svg width="3rem" height="3rem" class="spinner">
+                    <circle cx="50%" cy="50%" r="30%"/>
+                </svg>
+            </div>
+            <div v-show="showResult && !errorMsg">
+                <h6 v-show="searchResult.Name" class="text-black-50">{{ searchResult.Name }} ({{ searchResult.Symbol }})</h6>
+                <div class="d-flex justify-content-start align-items-end mb-1 prices">
+                    <h2 class="mr-3 mb-n1">{{ searchResult.LastPrice }}</h2>
+                    <h5 class="mr-1 mb-0"
+                        v-bind:class="[changeDrop ? 'text-danger' : 'text-success']">{{ searchResult.Change }}</h5>
+                    <h5 v-show="searchResult.ChangePercent" class="mb-0"
+                        v-bind:class="[changeDrop ? 'text-danger' : 'text-success']">({{ searchResult.ChangePercent }})</h5>
                 </div>
-                <hr class="bg-light my-1">
+                <p class="text-grey font-weight-custom timestamp">{{ searchResult.Timestamp }}</p>
+                <hr class="bg-light mt-2 mb-1">
+                <div v-for="{ item, key } in detailItems" v-bind:key="item">
+                    <div class="d-flex justify-content-between align-items-center px-1">
+                        <small class="text-grey font-weight-custom">{{ item }}</small>
+                        <small class="font-weight-bold">{{ searchResult[key] }}</small>
+                    </div>
+                    <hr class="bg-light my-1">
+                </div>
             </div>
         </div>
     </div>
@@ -62,12 +72,21 @@ export default {
         showResult() {
             const length = Object.keys(this.searchResult).length;
             return !!length;
+        },
+
+        changeDrop() {
+            if (this.searchResult.Change) {
+                return this.searchResult.Change.includes("-");
+            }
+
+            return false;
         }
     },
 
     methods: {
         async searchSecurity() {
             if (!this.searching) {
+                this.searching = true;
                 let controller = new AbortController();
 
                 setTimeout(() => {
@@ -105,8 +124,42 @@ export default {
         width: 60%;
     }
 
-    .timestamp {
-        font-size: 70%;
+    .contents {
+        min-width: 5rem;
+        min-height: 5rem;
+
+	    .overlay {
+            top: 0;
+            left: 0;
+		    background-color: rgba(255, 255, 255, .75);
+
+		    .spinner {
+			    fill: none;
+			    stroke: #908F8F;
+			    stroke-width: .25rem;
+			    stroke-dasharray: 120;
+			    stroke-dashoffset: 120;
+			    stroke-linecap: round;
+			    transform: rotateZ(-90deg);
+			    animation: spin 1s linear infinite;
+		    }
+	    }
+
+	    .timestamp {
+		    font-size: 70%;
+	    }
     }
+}
+
+@keyframes spin {
+	35% {
+		stroke-dashoffset: 50;
+		transform: rotateZ(-80deg);
+	}
+
+	80% {
+		stroke-dashoffset: 120;
+		transform: rotateZ(275deg);
+	}
 }
 </style>
